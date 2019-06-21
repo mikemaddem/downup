b6a5f3968e0dae7d1dec1a7131e0ee10d6280b4b
Mike M <mikemaddem@gmail.com>
2019-06-20 22:33:23 -0400

    users models and migrations

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+ACCOUNT_TYPES = [
+    ('admin', 'Admin'),
+    ('superadmin', 'Superadmin')
+]
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, related_name='useraccount', on_delete=models.CASCADE)
+    type = models.CharField(choices=ACCOUNT_TYPES, null=True, blank=True, max_length=255)
