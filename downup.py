be3f2fdf4924b3baef364f53129d9b0891fee86f
Mike M <mikemaddem@gmail.com>
2019-06-20 22:13:16 -0400

    i did stuff

--- downup.py
+++ downup.py
@@ -1,7 +1,40 @@
 import json
-from pprint import pprint
+import requests
+import pprint as pprint
+import objectpath
+
+get = []
+ping = []
+
 
 def read_config():
-	with open('config.json') as data_file:
-		data = json.load(data_file)
-	pprint(data)
+    with open('config.json') as data_file:
+        data = json.load(data_file)
+
+    if pprint.isreadable(data):
+        print('read config file')
+    # print(pprint.pformat(data))
+
+    for (x, y) in data.items():
+        if x == 'http.get':
+            for (z,a) in y.items():
+                for b in a.items():
+                    get.append(b[1])
+
+            # for a in z[]:
+            # print(a)
+        if x == 'ping':
+            for (z, a) in y.items():
+                for b in a.items():
+                    ping.append(b[1])
+
+
+def run():
+    with open("config.json") as datafile:
+        data = json.load(datafile)
+    jsonnn_tree = objectpath.Tree(data['http.get'])
+    result_tuple = tuple(jsonnn_tree.execute('$..duration'))
+    print(result_tuple)
+
+# read_config()
+run()
