2b7007b00518763422a4e5f3be230e499be9dec4
Mike M <mikemaddem@gmail.com>
2019-06-20 22:33:16 -0400

    tracker migrations and models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from user.models import UserProfile
 
-# Create your models here.
+
+class HTTPGetTracker(models.Model):
+    url = models.URLField(blank=True, null=True)
+    creator = models.ForeignKey(UserProfile, related_name='getcreator', on_delete=models.CASCADE)
+    priority = models.PositiveIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class PINGTracker(models.Model):
+    ip = models.CharField(default='0.0.0.0', max_length=255)
+    port = models.PositiveIntegerField(default=80)
+    creator = models.ForeignKey(UserProfile, related_name='pingcreator', on_delete=models.CASCADE)
+    priority = models.PositiveIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
